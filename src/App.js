076ae95977414bfdb4e1e15c8c9d24ee05ac3e3f
Abhirupa Mitra <mitra.abhirupa@gmail.com>
2023-04-12 13:02:41 +0530

    Fixing branches

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/nav.js';
 import HomePage from './components/homepage';
 import ContactMe from './components/contact';
+import AboutMe from './components/aboutMe';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent/>      
       <HomePage/>
+      <AboutMe/>
       <ContactMe/>
     </div>
   );
